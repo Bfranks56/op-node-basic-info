87625fc291c1b4bb0703c38a327c6c1ffc1b4a85
bfranks56 <bryant.franks@gmail.com>
2025-07-24 13:32:32 -0400

    just setting up index

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const http = require('node:http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    // put code here to handle requests
+    if (req.url === '/' || req.url === '/index.html') {
+        const filePath = path.join(__dirname, 'index.html');
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500);
+                res.end('Error loading page');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            };
+        });
+    } else {
+        // this isn't going to be final. just putting it here for now.
+        writeHead(404);
+        res.end('404 - Page Not Found');
+    }
 });
 
 server.listen(PORT, () => {
