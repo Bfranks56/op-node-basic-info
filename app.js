15f076526edf76b3b89d6e1d11cc316111df51a6
bfranks56 <bryant.franks@gmail.com>
2025-07-31 10:04:48 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('node:path');
-const urlMap = require('./urlMap.Dto.js');  // Import the URL map
+const urlMap = require('./urlMap.Dto.js');
 const app = express();
 
-
+// TODO: see if I can do this with one call using the map.
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, urlMap['/']))
 });
@@ -16,8 +16,7 @@ app.get("/contact-me", (req, res) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, urlMap['/404'])); // Serve 404 page for unmatched routes
-});
+    res.status(404).sendFile(path.join(__dirname, urlMap['/404']));
 
 const PORT = process.env.PORT;
 
